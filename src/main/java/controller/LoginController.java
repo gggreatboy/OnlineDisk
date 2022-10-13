553f2822340143ea081a0abd40ffa20e44da4fe7
zong <zong@zong.com>
2022-10-13 17:20:08 +0800

    注册页面thymeleaf化

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javassist.expr.NewArray;
 import pojo.loginBean;
 
 @Controller
