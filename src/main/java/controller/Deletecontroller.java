f9afe63e62a11eb4de4641bc81b71da427dc8a40
WYQ <1836022119@qq.com>
2022-10-13 13:17:24 +0800

    13:17 10/13

---
+++ src/main/java/controller/Deletecontroller.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Deletecontroller {
+	@RequestMapping("/delete2")
+	public String todelete(HttpSession session,HttpServletRequest request, 
+			@RequestParam("filename") String filename,
+			@RequestParam("filepath") String filepath,Model model) {
+		    Path path = Paths.get(filepath+filename);
+		    try {
+				Files.delete(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+        return "redirect:http://localhost:8080/OnlineDisk/";
+	}
+}
